6814966a6b8db6726baea8600157ef254bc14b16
so1eax <s1xs9wa@gmail.com>
2024-10-19 12:27:47 +0200

    updated triggerbot

--- gta5rp/gta5rp.cpp
+++ gta5rp/gta5rp.cpp
@@ -1,17 +1,23 @@
 #include <Windows.h>
 #include <cstdio>
 #include <iostream>
-#include "wtypes.h"
-
+#include <wtypes.h>
 #include <chrono>
 #include <thread>
+#include <atomic>
+#include <cstdlib>
 
+using namespace std;
 using namespace std::chrono_literals;
-int triggerBot() {
+
+std::atomic<bool> triggerBotRunning(false);
+std::atomic<bool> strafeMacroRunning(false);
+
+void triggerBot() {
     RECT desktop;
     const HWND hDesktop = GetDesktopWindow();
 
-    while (true) {
+    while (triggerBotRunning.load()) {
         GetWindowRect(hDesktop, &desktop);
 
         if (!desktop.right || !desktop.bottom)
@@ -21,7 +27,7 @@ int triggerBot() {
         if (!dc)
             continue;
 
-        COLORREF pxcolor = GetPixel(dc, desktop.right / 2, desktop.bottom / 2);
+        COLORREF pxcolor = GetPixel(dc, (desktop.right / 2), (desktop.bottom / 2));
         ReleaseDC(NULL, dc);
         if (pxcolor == CLR_INVALID)
             continue;
@@ -31,30 +37,120 @@ int triggerBot() {
         int blue = GetBValue(pxcolor);
 
         if (GetAsyncKeyState(VK_RBUTTON)) {
-            std::this_thread::sleep_for(10ms);
-            if (red > 150 && green < 100 && blue < 100)
-            {
+            if (red > 150 && green < 100 && blue < 100) {
                 INPUT input = { 0 };
                 input.type = INPUT_MOUSE;
                 input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
 
                 SendInput(1, &input, sizeof(INPUT));
 
-                std::this_thread::sleep_for(1ms);
+                std::this_thread::sleep_for(0.5ms);
 
                 input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
                 SendInput(1, &input, sizeof(INPUT));
             }
         }
-        else
-            std::this_thread::sleep_for(500ms);
-
     }
+}
 
-    return 0;
+
+
+void simulateKeyPress(WORD key) {
+    INPUT input = { 0 };
+    input.type = INPUT_KEYBOARD;
+    input.ki.wVk = key;
+
+    SendInput(1, &input, sizeof(INPUT));
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(90));
+
+    input.ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, &input, sizeof(INPUT));
+}
+void strafeMacro() {
+    while (strafeMacroRunning.load()) {
+        simulateKeyPress(0x57);
+        simulateKeyPress(0x41);
+        simulateKeyPress(0x53);
+        simulateKeyPress(0x44);
+    }
+}
+
+
+bool TbRunning = false;
+bool SmRunning = false;
+void statusThread() {
+    while (true)
+    {
+        if (TbRunning) cout << "TriggerBot status: \033[32m running \033[0m" << endl;
+        else cout << "TriggerBot status: \033[31m stopped \033[0m" << endl;
+
+        if (SmRunning) cout << "StrafeMacro status: \033[32m running \033[0m" << endl;
+        else cout << "StrafeMacro status: \033[31m stopped \033[0m" << endl;
+
+        this_thread::sleep_for(500ms);
+
+        system("cls");
+    }
 }
 
 int main()
 {
-    triggerBot();
+    std::thread tbThread;
+    std::thread smThread;
+
+    auto triggerbotKey = 0x58;
+    auto strafemacroKey = 0x45;
+
+    bool TbKeyPressed = false;
+    bool SmKeyPressed = false;
+
+    thread statusLoop(statusThread);
+
+    while (true) {
+        if (GetAsyncKeyState(triggerbotKey) & 0x8000) {
+            if (!TbKeyPressed) {
+                if (!TbRunning && !triggerBotRunning.load()) {
+                    triggerBotRunning.store(true);
+                    tbThread = thread(triggerBot);
+                    TbRunning = true;
+                }
+                else {
+                    triggerBotRunning.store(false);
+                    if (tbThread.joinable())
+                        tbThread.join();
+
+                    TbRunning = false;
+                }
+                TbKeyPressed = true;
+            }
+        }
+        else if (TbKeyPressed)
+            TbKeyPressed = false;
+
+
+
+        else if (GetAsyncKeyState(strafemacroKey) & 0x8000) {
+            if (!SmKeyPressed) {
+                if (!SmRunning && !strafeMacroRunning.load()) {
+                    strafeMacroRunning.store(true);
+                    smThread = thread(strafeMacro);
+                    SmRunning = true;
+                }
+                else {
+                    strafeMacroRunning.store(false);
+                    if (smThread.joinable())
+                        smThread.join();
+
+                    SmRunning = false;
+                }
+                SmKeyPressed = true;
+            }
+        }
+        else if (SmKeyPressed)
+            SmKeyPressed = false;
+        this_thread::sleep_for(100ms);
+    }
+
+    return 0;
 }
